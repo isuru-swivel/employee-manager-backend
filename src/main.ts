5cf32191ec55e811f9166911bbf3a3f7fff6b5f0
Isuru Madusanka <isurum@swivelgroup.com.au>
2023-03-01 13:49:36 +0530

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'https://employee-manager-frontend-lovat.vercel.app',
+    optionsSuccessStatus: 200,
+    preflightContinue: true,
+    credentials: true,
+  });
   await app.listen(3001);
 }
 
