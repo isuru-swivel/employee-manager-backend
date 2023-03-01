c18ea54dafe18f7fa0405ba9b84e56e0d29cb4f8
Isuru Madusanka <isurum@swivelgroup.com.au>
2023-03-01 12:23:52 +0530

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3001);
 }
